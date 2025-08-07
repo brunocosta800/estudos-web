6d7849170a354639b7857dd8124eeaac8153bfef
Bruno Costa Dourado <brunoc.dourado10@gmail.com>
2025-08-07 18:09:07 -0300

    new file:   aula11/index.js

---
+++ aula11/index.js
@@ -0,0 +1,36 @@
+/*
+  Operadores Aritméticos:
+  
+  + -> Adição / Concatenação
+  - -> Subtração
+  * -> Multiplicação
+  / -> Divisão
+  ** -> Potenciação
+  % -> Resto da divisão
+
+  ++ e -- são operadores incrementais;
+*/
+
+/*
+  Precedência Aritmética:
+  ()
+  **
+  * / %
+  + -
+
+*/
+
+const num1 = 5;
+const num2 = 2;
+console.log(num1 + num2);
+
+const num3 = 10;
+const num4 = 10;
+console.log(num3 + num4);
+
+console.log(num1 + num2 * num3); // Segue a precedência de expressões numéricas
+
+let contador = 2;
+contador **= 10;
+contador **= 10;
+console.log(contador);
